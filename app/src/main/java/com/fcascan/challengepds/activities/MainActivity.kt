02cdea94b35078c1d136f9026d3ad7556e2b6f8a
fcastrocanosa <fcastrocanosa@asjservicios.com>
2023-12-02 19:06:52 -0300

    Factorial function logic implemented

--- app/src/main/java/com/fcascan/challengepds/activities/MainActivity.kt
+++ app/src/main/java/com/fcascan/challengepds/activities/MainActivity.kt
@@ -2,8 +2,10 @@ package com.fcascan.challengepds.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.fcascan.challengepds.R
 import com.fcascan.challengepds.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private val _TAG = "FCC#MainActivity"
@@ -16,10 +18,88 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Lifecycle Log:
+        Log.d(_TAG, "onCreate - TimeStamp: ${System.currentTimeMillis()}")
+        //TODO almacenar en Room este lifecycle log
+
         //View Binding:
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
+        //Loading State Off:
+        binding.progressBar.visibility = android.view.View.GONE
+
+        //Button Factorial:
+        binding.btnFactorial.setOnClickListener {
+            if (binding.editTextNumber.text.toString() == "") {
+                binding.tvFactorialResult.text = "Please enter a number"
+                return@setOnClickListener
+            }
+            val number = binding.editTextNumber.text.toString().toLong()
+            if (number < 0)
+                binding.tvFactorialResult.text = "Math Error"
+            else {
+                val factorial = myFactorial(number)
+                binding.tvFactorialResult.text = "$number!  =  $factorial"
+                //TODO considerar el caso de que el número sea muy grande
+            }
+        }
+
+        //Button Recursive Factorial:
+        binding.btnFactorialV2.setOnClickListener {
+            if (binding.editTextNumber.text.toString() == "") {
+                binding.tvFactorialResult.text = "Please enter a number"
+                return@setOnClickListener
+            }
+            val number = binding.editTextNumber.text.toString().toLong()
+            if (number < 0)
+                binding.tvFactorialResult.text = "Math Error"
+            else {
+                val factorial = myRecursiveFactorial(number)
+                binding.tvFactorialResult.text = "$number!  =  $factorial"
+                //TODO considerar el caso de que el número sea muy grande
+            }
+        }
+
+        //Button Time:
+        binding.btnTime.setOnClickListener {
+            val time = System.currentTimeMillis()
+            Snackbar.make(binding.root, "Time: $time", Snackbar.LENGTH_LONG).show()
+            //TODO esta logica tiene que ser mediante una pegada a una API
+        }
+
+        //Edit Text Enter Pressed:
+        binding.editTextNumber.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == android.view.KeyEvent.KEYCODE_ENTER && event.action == android.view.KeyEvent.ACTION_UP) {
+                binding.btnFactorial.performClick()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //Lifecycle Log:
+        Log.d(_TAG, "onCreate - TimeStamp: ${System.currentTimeMillis()}")
+        //TODO almacenar en Room este lifecycle log
+
+//        sharedViewModel.screenState.removeObservers(this)
+        finish()
+    }
+
+    fun myFactorial(number: Long): Long {
+        var result = 1L
+        for (i in 1L..number) {
+            result *= i
+        }
+        return result
+    }
+
+    fun myRecursiveFactorial(number: Long): Long {
+        if (number == 0L) return 1
+        return number * myRecursiveFactorial(number - 1)
+    }
 }
